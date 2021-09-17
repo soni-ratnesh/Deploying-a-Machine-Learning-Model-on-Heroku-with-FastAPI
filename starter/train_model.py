86daac81d03af6cb5c3b9310032abe7f0158be30
soni-ratnesh <ratneshk.99@gmail.com>
2021-09-17 10:01:30 +0530

    Add model training and saving

--- starter/train_model.py
+++ starter/train_model.py
@@ -1,28 +1,67 @@
 # Script to train machine learning model.
+import os
+import pandas as pd
 
+from joblib import dump, load
 from sklearn.model_selection import train_test_split
+from ml.data import process_data
+from ml.model import train_model, compute_model_metrics
+
+
+def get_train_test_data(root_path):
+    """
+    Get data for training and testing
+
+    Parameters
+    ----------
+    root_path
+
+    Returns
+    -------
+    train_df , test_df
+    """
+    data = pd.read_csv(f"data/clean/census.csv")
+    train_df, test_df = train_test_split(data, test_size=0.20)
+
+    return train_df, test_df
+
 
 # Add the necessary imports for the starter code.
-
+# def train_test_model(stages):
+#     if os.path.exists()
 # Add code to load in the data.
 
-# Optional enhancement, use K-fold cross validation instead of a train-test split.
-train, test = train_test_split(data, test_size=0.20)
 
-cat_features = [
-    "workclass",
-    "education",
-    "marital-status",
-    "occupation",
-    "relationship",
-    "race",
-    "sex",
-    "native-country",
-]
-X_train, y_train, encoder, lb = process_data(
-    train, categorical_features=cat_features, label="salary", training=True
-)
+def train_save_model(train, cat_features, root_path):
+    """
+    
+    Parameters
+    ----------
+    root_path
+    train
+    cat_features
 
-# Proces the test data with the process_data function.
+    Returns
+    -------
 
-# Train and save a model.
+    """
+    x_train, y_train, encoder, lb = process_data(
+        train, categorical_features=cat_features, label="salary", training=True
+    )
+    # train model
+    trained_model = train_model(x_train, y_train)
+    # save model
+    dump(trained_model, f"{root_path}data/model/model.joblib")
+    dump(encoder, f"{root_path}/data/model/encoder.joblib")
+    dump(lb, f"{root_path}/data/model/lb.joblib")
+
+# # Proces the test data with the process_data function.
+# X_test, y_test, _, _ = process_data(test, categorical_features=cat_features, label="salary", training=False,
+#                                     encoder=encoder, lb=lb)
+# trained_model = load("data/model/model.joblib")
+# encoder = load("data/model/encoder.joblib")
+# lb = load("data/model/lb.joblib")
+# 
+# y_preds = trained_model.predict(X_test)
+# 
+# prc, rcl, fb = compute_model_metrics(y_test, y_preds)

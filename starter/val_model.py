524d04dc5a55c8dd4f285aa17a7c6f318b0e5972
Ratnesh Kumar <ratneshk.99@gmail.com>
2021-09-17 21:41:15 +0530

    Update val_model.py

--- starter/val_model.py
+++ starter/val_model.py
@@ -1,6 +1,5 @@
 from joblib import load
-from .ml.data import process_data
-from .ml.model import compute_model_metrics, compute_score_per_slice
+from .ml.model import compute_score_per_slice
 
 
 def val_model(test_df, cat_features, root_dir):
@@ -21,5 +20,10 @@ def val_model(test_df, cat_features, root_dir):
     encoder = load(f"{root_dir}/model/encoder.joblib")
     lb = load(f"{root_dir}/model/lb.joblib")
 
-    compute_score_per_slice(trained_model, test_df, encoder, lb, cat_features, root_dir)
-
+    compute_score_per_slice(
+        trained_model,
+        test_df,
+        encoder,
+        lb,
+        cat_features,
+        root_dir)

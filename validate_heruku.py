58c0866fa4cbb3c2ef895f9cd9bb864341a2717d
Ratnesh Kumar <ratneshk.99@gmail.com>
2021-09-17 21:54:09 +0530

    Update validate_heruku.py

--- validate_heruku.py
+++ validate_heruku.py
@@ -3,18 +3,20 @@ Heroku Api test script
 """
 import requests
 
-data = {'age': 19,
-         'workclass': 'Private',
-         'fnlgt': 149184,
-         'education': 'HS-grad',
-         'marital_status': 'Never-married',
-         'occupation': 'Prof-specialty',
-         'relationship': 'Not-in-family',
-         'race': 'White',
-         'sex': 'Male',
-         'hoursPerWeek': 60,
-         'nativeCountry': 'United-States'
-         }
+data = {
+            'age': 19,
+            'workclass': 'Private',
+            'fnlgt': 149184,
+            'education': 'HS-grad',
+            'marital_status': 'Never-married',
+            'occupation': 'Prof-specialty',
+            'relationship': 'Not-in-family',
+            'race': 'White',
+            'sex': 'Male',
+            'hoursPerWeek': 60,
+            'nativeCountry': 'United-States'
+    }
+
 r = requests.post('https://cdudacity.herokuapp.com/', json=data)
 
 assert r.status_code == 200

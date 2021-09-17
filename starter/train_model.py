ab346e2a063cd85cc585f26a7b2782c2720d906e
soni-ratnesh <ratneshk.99@gmail.com>
2021-09-17 10:16:48 +0530

    Refactor train_model.

--- starter/train_model.py
+++ starter/train_model.py
@@ -1,11 +1,11 @@
 # Script to train machine learning model.
-import os
+
 import pandas as pd
 
-from joblib import dump, load
+from joblib import dump
 from sklearn.model_selection import train_test_split
 from .ml.data import process_data
-from .ml.model import train_model, compute_model_metrics
+from .ml.model import train_model
 
 
 def get_train_test_data(root_path):
@@ -26,15 +26,9 @@ def get_train_test_data(root_path):
     return train_df, test_df
 
 
-# Add the necessary imports for the starter code.
-# def train_test_model(stages):
-#     if os.path.exists()
-# Add code to load in the data.
-
-
 def train_save_model(train, cat_features, root_path):
     """
-    
+    Train and save ml model
     Parameters
     ----------
     root_path
@@ -54,14 +48,3 @@ def train_save_model(train, cat_features, root_path):
     dump(trained_model, f"{root_path}/model/model.joblib")
     dump(encoder, f"{root_path}/model/encoder.joblib")
     dump(lb, f"{root_path}/model/lb.joblib")
-
-# # Proces the test data with the process_data function.
-# X_test, y_test, _, _ = process_data(test, categorical_features=cat_features, label="salary", training=False,
-#                                     encoder=encoder, lb=lb)
-# trained_model = load("data/model/model.joblib")
-# encoder = load("data/model/encoder.joblib")
-# lb = load("data/model/lb.joblib")
-# 
-# y_preds = trained_model.predict(X_test)
-# 
-# prc, rcl, fb = compute_model_metrics(y_test, y_preds)

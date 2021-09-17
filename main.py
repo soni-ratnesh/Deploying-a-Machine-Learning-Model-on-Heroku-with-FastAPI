ad41d83d52f59203aae6b0fc9292028310084679
soni-ratnesh <ratneshk.99@gmail.com>
2021-09-17 09:03:12 +0530

    Add main entrypoint for train, test, score model

--- main.py
+++ main.py
@@ -1,1 +1,41 @@
-# Put the code for your API here.
+import argparse
+import hydra
+import logging
+
+from omegaconf import DictConfig
+
+_steps = [
+    "data_cleaning",
+    "train_test_model",
+    "check_score"
+]
+
+
+@hydra.main(config_name='config')
+def go(config: DictConfig):
+    """
+    Run pipeline stages
+    """
+    logging.basicConfig(level=logging.INFO)
+
+    root_path = hydra.utils.get_original_cwd()
+
+    # Steps to execute
+    steps_par = config['main']['steps']
+    active_steps = steps_par.split(",") if steps_par != "all" else _steps
+
+    if "data_cleaning" in active_steps:
+        logging.info("Cleaning and saving raw_data")
+
+    if "train_test_model" in active_steps:
+        logging.info("Train/Test model procedure started")
+
+    if "check_score" in active_steps:
+        logging.info("Score check procedure started")
+
+
+if __name__ == "__main__":
+    """
+    Main entrypoint
+    """
+    go()

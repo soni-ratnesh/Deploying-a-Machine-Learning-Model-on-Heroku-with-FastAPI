a65af8978ad8656f5dc82ad30d67f63e27e30f75
soni-ratnesh <ratneshk.99@gmail.com>
2021-09-17 13:43:10 +0530

    Add inference testcase

--- test_case/conftest.py
+++ test_case/conftest.py
@@ -51,3 +51,41 @@ def test_data(clean_data):
     """
     df = clean_data['salary']
     return df
+
+
+@pytest.fixture
+def inference_data_low():
+    data_dict = {'age': 19,
+                 'workclass': 'Private',
+                 'fnlgt': 77516,
+                 'education': 'HS-grad',
+                 'marital-status': 'Never-married',
+                 'occupation': 'Own-child',
+                 'relationship': 'Husband',
+                 'race': 'Black',
+                 'sex': 'Male',
+                 'hours-per-week': 40,
+                 'native-country': 'United-States'
+                 }
+    df = pd.DataFrame(data=data_dict.values(),
+                      index=data_dict.keys()).T
+    return df
+
+
+@pytest.fixture
+def inference_data_high():
+    data_dict = {'age': 33,
+                 'workclass': 'Private',
+                 'fnlgt': 149184,
+                 'education': 'HS-grad',
+                 'marital-status': 'Never-married',
+                 'occupation': 'Prof-specialty',
+                 'relationship': 'Not-in-family',
+                 'race': 'White',
+                 'sex': 'Male',
+                 'hours-per-week': 60,
+                 'native-country': 'United-States'
+                 }
+    df = pd.DataFrame(data=data_dict.values(),
+                      index=data_dict.keys()).T
+    return df
